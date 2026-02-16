036dcf64b3220b9f5ce17715c334651e8404438a
Velislav Donchev <156554141+velislav088@users.noreply.github.com>
2026-02-17 01:38:24 +0200

    Scaffold airport controller

---
+++ GoAir/Controllers/AirportsController.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using GoAir.Data;
+using GoAir.Models;
+
+namespace GoAir.Controllers
+{
+    public class AirportsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AirportsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Airports
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Airports.ToListAsync());
+        }
+
+        // GET: Airports/Details
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var airport = await _context.Airports
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (airport == null)
+            {
+                return NotFound();
+            }
+
+            return View(airport);
+        }
+
+        // GET: Airports/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Airports/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,City,Terminals")] Airport airport)
+        {
+            if (ModelState.IsValid)
+            {
+                airport.Id = Guid.NewGuid();
+                _context.Add(airport);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(airport);
+        }
+
+        // GET: Airports/Edit
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var airport = await _context.Airports.FindAsync(id);
+            if (airport == null)
+            {
+                return NotFound();
+            }
+            return View(airport);
+        }
+
+        // POST: Airports/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,City,Terminals")] Airport airport)
+        {
+            if (id != airport.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(airport);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AirportExists(airport.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(airport);
+        }
+
+        // GET: Airports/Delete
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var airport = await _context.Airports
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (airport == null)
+            {
+                return NotFound();
+            }
+
+            return View(airport);
+        }
+
+        // POST: Airports/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var airport = await _context.Airports.FindAsync(id);
+            if (airport != null)
+            {
+                _context.Airports.Remove(airport);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AirportExists(Guid id)
+        {
+            return _context.Airports.Any(e => e.Id == id);
+        }
+    }
+}

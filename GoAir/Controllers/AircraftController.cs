311709f15c1e06df2f302047e5333739bb7644c2
Velislav Donchev <156554141+velislav088@users.noreply.github.com>
2026-02-17 01:38:11 +0200

    Scaffold aircraft controller

---
+++ GoAir/Controllers/AircraftController.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using GoAir.Data;
+using GoAir.Models;
+
+namespace GoAir.Controllers
+{
+    public class AircraftController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AircraftController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Aircraft
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Aircrafts.ToListAsync());
+        }
+
+        // GET: Aircraft/Details
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aircraft = await _context.Aircrafts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aircraft == null)
+            {
+                return NotFound();
+            }
+
+            return View(aircraft);
+        }
+
+        // GET: Aircraft/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Aircraft/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Model,Manufacturer,Capacity")] Aircraft aircraft)
+        {
+            if (ModelState.IsValid)
+            {
+                aircraft.Id = Guid.NewGuid();
+                _context.Add(aircraft);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aircraft);
+        }
+
+        // GET: Aircraft/Edit
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aircraft = await _context.Aircrafts.FindAsync(id);
+            if (aircraft == null)
+            {
+                return NotFound();
+            }
+            return View(aircraft);
+        }
+
+        // POST: Aircraft/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Model,Manufacturer,Capacity")] Aircraft aircraft)
+        {
+            if (id != aircraft.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(aircraft);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AircraftExists(aircraft.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aircraft);
+        }
+
+        // GET: Aircraft/Delete
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aircraft = await _context.Aircrafts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aircraft == null)
+            {
+                return NotFound();
+            }
+
+            return View(aircraft);
+        }
+
+        // POST: Aircraft/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var aircraft = await _context.Aircrafts.FindAsync(id);
+            if (aircraft != null)
+            {
+                _context.Aircrafts.Remove(aircraft);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AircraftExists(Guid id)
+        {
+            return _context.Aircrafts.Any(e => e.Id == id);
+        }
+    }
+}

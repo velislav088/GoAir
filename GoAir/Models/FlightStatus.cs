5ccf032b959a4c998c56ef79eb2a86b5edac3394
Velislav Donchev <156554141+velislav088@users.noreply.github.com>
2026-02-17 02:45:12 +0200

    Add display attributes to flight status enum

--- GoAir/Models/FlightStatus.cs
+++ GoAir/Models/FlightStatus.cs
@@ -1,11 +1,22 @@
-﻿namespace GoAir.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GoAir.Models
 {
     public enum FlightStatus
     {
+        [Display(Name = "Checking In")]
         CheckingIn = 0,
+
+        [Display(Name = "Boarding")]
         Boarding = 1,
+
+        [Display(Name = "Departed")]
         Departed = 2,
+
+        [Display(Name = "Delayed")]
         Delayed = 3,
+
+        [Display(Name = "Deboarding")]
         Deboarding = 4
     }
 }
